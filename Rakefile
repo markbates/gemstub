9f053388dc06156bcb48bc23a1d4ae83107493c2
Mark Bates <mark@markbates.com>
2010-01-15 13:50:57 -0500

    Got rid of support for releasing to RubyForge. It's no longer relevant with Gemcutter.

--- Rakefile
+++ Rakefile
@@ -12,7 +12,7 @@ require 'spec/rake/spectask'
 
 @gem_spec = Gem::Specification.new do |s|
   s.name = 'gemstub'
-  s.version = '1.5.7'
+  s.version = '1.6.0'
   s.author = "Mark Bates"
   s.email = "mark@markbates.com"
   s.homepage = "http://www.mackframework.com"
@@ -63,33 +63,33 @@ desc "Release the gem"
 task :release => :install do |t|
   gem_pkg = File.join("pkg", "#{@gem_spec.name}-#{@gem_spec.version}.gem")
   system "gem push #{gem_pkg}"
-  begin
-    ac_path = File.join(ENV["HOME"], ".rubyforge", "auto-config.yml")
-    if File.exists?(ac_path)
-      fixed = File.open(ac_path).read.gsub("  ~: {}\n\n", '')
-      fixed.gsub!(/    !ruby\/object:Gem::Version \? \n.+\n.+\n\n/, '')
-      puts "Fixing #{ac_path}..."
-      File.open(ac_path, "w") {|f| f.puts fixed}
-    end
-    begin
-      rf = RubyForge.new
-      rf.configure
-      rf.login
-      rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
-    rescue Exception => e
-      if e.message.match("Invalid package_id") || e.message.match("no <package_id> configured for")
-        puts "You need to create the package!"
-        rf.create_package(@gem_spec.rubyforge_project, @gem_spec.name)
-        rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
-      else
-        raise e
-      end
-    end
-  rescue Exception => e
-    if e.message == "You have already released this version."
-      puts e
-    else
-      raise e
-    end
-  end
+  # begin
+  #   ac_path = File.join(ENV["HOME"], ".rubyforge", "auto-config.yml")
+  #   if File.exists?(ac_path)
+  #     fixed = File.open(ac_path).read.gsub("  ~: {}\n\n", '')
+  #     fixed.gsub!(/    !ruby\/object:Gem::Version \? \n.+\n.+\n\n/, '')
+  #     puts "Fixing #{ac_path}..."
+  #     File.open(ac_path, "w") {|f| f.puts fixed}
+  #   end
+  #   begin
+  #     rf = RubyForge.new
+  #     rf.configure
+  #     rf.login
+  #     rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
+  #   rescue Exception => e
+  #     if e.message.match("Invalid package_id") || e.message.match("no <package_id> configured for")
+  #       puts "You need to create the package!"
+  #       rf.create_package(@gem_spec.rubyforge_project, @gem_spec.name)
+  #       rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
+  #     else
+  #       raise e
+  #     end
+  #   end
+  # rescue Exception => e
+  #   if e.message == "You have already released this version."
+  #     puts e
+  #   else
+  #     raise e
+  #   end
+  # end
 end
--- lib/gemstub.rb
+++ lib/gemstub.rb
@@ -8,7 +8,7 @@ require 'rake/clean'
 require 'rake/testtask'
 require 'rake/rdoctask'
 require 'find'
-require 'rubyforge'
+# require 'rubyforge'
 require 'rubygems'
 require 'rubygems/gem_runner'
 require 'spec'
@@ -90,35 +90,35 @@ module Gemstub
       task :release => :install do |t|
         gem_pkg = File.join("pkg", "#{@gem_spec.name}-#{@gem_spec.version}.gem")
         system "gem push #{gem_pkg}"
-        begin
-          ac_path = File.join(ENV["HOME"], ".rubyforge", "auto-config.yml")
-          if File.exists?(ac_path)
-            fixed = File.open(ac_path).read.gsub("  ~: {}\n\n", '')
-            fixed.gsub!(/    !ruby\/object:Gem::Version \? \n.+\n.+\n\n/, '')
-            puts "Fixing #{ac_path}..."
-            File.open(ac_path, "w") {|f| f.puts fixed}
-          end
-          begin
-            rf = RubyForge.new
-            rf.configure
-            rf.login
-            rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
-          rescue Exception => e
-            if e.message.match("Invalid package_id") || e.message.match("no <package_id> configured for")
-              puts "You need to create the package!"
-              rf.create_package(@gem_spec.rubyforge_project, @gem_spec.name)
-              rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
-            else
-              raise e
-            end
-          end
-        rescue Exception => e
-          if e.message == "You have already released this version."
-            puts e
-          else
-            raise e
-          end
-        end
+        # begin
+        #           ac_path = File.join(ENV["HOME"], ".rubyforge", "auto-config.yml")
+        #           if File.exists?(ac_path)
+        #             fixed = File.open(ac_path).read.gsub("  ~: {}\n\n", '')
+        #             fixed.gsub!(/    !ruby\/object:Gem::Version \? \n.+\n.+\n\n/, '')
+        #             puts "Fixing #{ac_path}..."
+        #             File.open(ac_path, "w") {|f| f.puts fixed}
+        #           end
+        #           begin
+        #             rf = RubyForge.new
+        #             rf.configure
+        #             rf.login
+        #             rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
+        #           rescue Exception => e
+        #             if e.message.match("Invalid package_id") || e.message.match("no <package_id> configured for")
+        #               puts "You need to create the package!"
+        #               rf.create_package(@gem_spec.rubyforge_project, @gem_spec.name)
+        #               rf.add_release(@gem_spec.rubyforge_project, @gem_spec.name, @gem_spec.version, gem_pkg)
+        #             else
+        #               raise e
+        #             end
+        #           end
+        #         rescue Exception => e
+        #           if e.message == "You have already released this version."
+        #             puts e
+        #           else
+        #             raise e
+        #           end
+        #         end
       end
       
       task :readme do
