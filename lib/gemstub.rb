e7608c592a5f9a98014277425be5b6aca579e1a4
Mark Bates <mark@markbates.com>
2010-08-12 14:32:12 -0400

    Gem stuff.

--- lib/gemstub.rb
+++ lib/gemstub.rb
@@ -35,7 +35,7 @@ module Gemstub
       if rspec?
         # rake
         desc 'Run specifications'
-        Spec::Rake::SpecTask.new(:default) do |t|
+        Rspec::Core::RakeTask.new(:default) do |t|
           opts = File.join('spec', 'spec.opts')
           t.spec_opts << '--options' << opts if File.exists?(opts)
           t.spec_files = Dir.glob('spec/**/*_spec.rb')
